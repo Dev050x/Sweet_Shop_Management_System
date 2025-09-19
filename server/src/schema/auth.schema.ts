b98d849839b285a348a8ebf13cd87b4999d56ac8
0xDev05 <divpatel1274@gmail.com>
2025-09-19 16:13:39 +0530

    feat: implement name schema validation

--- server/src/schema/auth.schema.ts
+++ server/src/schema/auth.schema.ts
@@ -2,8 +2,7 @@ import { z } from "zod";
 
 
 export const registerSchema = z.object({
-  name: z.string(),
+  name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ , "Email must be in correct form"),
   password: z.string()
-  .min(8, "Password must be at least 8 characters")
 });
