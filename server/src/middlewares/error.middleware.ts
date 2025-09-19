0f2ea07f3be01279d2501abe624b3f5f89d0336e
0xDev05 <divpatel1274@gmail.com>
2025-09-19 18:11:23 +0530

    refactor: improve error handling for non-existing email

--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -13,6 +13,23 @@ export const errorHandler = (error: any, req: Request, res: Response, next: Next
         });
     }
 
+    if (error.message === "Email already registered") {
+        return res.status(409).json({
+          success: false,
+          message: error.message,
+          errors:error
+        });
+    }
+
+    if (error.message === "User not found" ) {
+        return res.status(401).json({
+          success: false,
+          message: error.message,
+          errors:error
+        });
+    }
+
+
     return res.status(500).json({
         success: false,
         message: "internal server error",
