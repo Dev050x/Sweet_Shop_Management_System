cf2b5083e96900036c6a408ab61192ae877484d3
0xDev05 <divpatel1274@gmail.com>
2025-09-19 16:14:05 +0530

    refactor: improve schema validation structure

--- server/src/schema/auth.schema.ts
+++ server/src/schema/auth.schema.ts
@@ -5,4 +5,9 @@ export const registerSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters"),
   email: z.string().regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ , "Email must be in correct form"),
   password: z.string()
+  .min(8, "Password must be at least 8 characters")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Password must contain at least one number")
+  .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character"),
 });
