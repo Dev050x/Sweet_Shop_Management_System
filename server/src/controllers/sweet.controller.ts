d78445d32b2e2bff006c680b2464e3f96387c4be
0xDev05 <divpatel1274@gmail.com>
2025-09-19 20:19:54 +0530

    feat: add service layer for creating sweet

--- server/src/controllers/sweet.controller.ts
+++ server/src/controllers/sweet.controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { sweetSchema } from "../schema/sweet.schema";
-
+import * as sweetService from "../services/sweet.service";
 
 //add sweet controller
 export const addSweet = async (req: Request, res: Response, next: NextFunction) => {
@@ -11,6 +11,16 @@ export const addSweet = async (req: Request, res: Response, next: NextFunction) 
     if (!validatedData.success) {
       return next(validatedData.error);
     }
+
+    //calling service
+     // call service to add sweet
+    const sweet = await sweetService.addSweet(
+      validatedData.data.name,
+      validatedData.data.category,
+      validatedData.data.price,
+      validatedData.data.quantity
+    );
+
    } catch (error) {
       next(error);
    }
---
+++ server/src/services/sweet.service.ts
@@ -0,0 +1,9 @@
+import {prisma} from "../utils/prisma";
+import { SweetCategory } from "@prisma/client";
+
+//add sweets 
+export const addSweet = async (name: string, category: SweetCategory, price: number, quantity: number) => {
+  return prisma.sweet.create({
+    data: { name, category, price, quantity },
+  });
+};
