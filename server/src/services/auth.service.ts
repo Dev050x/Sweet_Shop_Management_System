a9b546be42f4da8351a7711af4a7511598d3d509
0xDev05 <divpatel1274@gmail.com>
2025-09-19 16:33:05 +0530

    feat: hash password and create user in service layer

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,8 +1,17 @@
 import {prisma} from "../utils/prisma";
+import bcrypt from "bcryptjs"
 
 export const registerUser = async (name: string, email: string, password: string) => {
   
   const existing = await prisma.user.findUnique({ where: { email } });
   if (existing) throw new Error("Email already registered");
 
+  const hashedPassword = await bcrypt.hash(password, 10);
+  return prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 };
