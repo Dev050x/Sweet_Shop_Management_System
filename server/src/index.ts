b72153c5ac9eeedd1c93fc4095300f25318a64dd
0xDev05 <divpatel1274@gmail.com>
2025-09-20 16:37:48 +0530

    chore(server): add development CORS configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,11 @@ import express from "express";
 import authRouter from "./routes/auth.routes";
 import { errorHandler } from "./middlewares/error.middleware";
 import sweetRouter from "./routes/sweet.routes";
+import  cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api",authRouter);
